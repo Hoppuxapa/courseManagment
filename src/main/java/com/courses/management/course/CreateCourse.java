4963066f932994e1f995d126b5f364635e3b296a
Ruslan <hoppuxapa@gmail.com>
2020-03-10 17:27:51 +0200

    Add CourseDAO interface.

--- src/main/java/com/courses/management/course/CreateCourse.java
+++ src/main/java/com/courses/management/course/CreateCourse.java
@@ -2,21 +2,22 @@ package com.courses.management.course;
 
 import com.courses.management.common.Command;
 import com.courses.management.common.DataAccessObject;
-import com.courses.management.common.DatabaseConnector;
 import com.courses.management.common.View;
 
 public class CreateCourse implements Command {
     private final View view;
     private DataAccessObject<Course> courseDAO;
+
     public CreateCourse(View view) {
         this.view = view;
-        DatabaseConnector connector = new DatabaseConnector();
-        courseDAO = new CourseDAO(connector.getConnection());
+        courseDAO = new CourseDAOImpl();
     }
+
     @Override
     public String command() {
         return "create_course";
     }
+
     @Override
     public void process() {
         view.write("Enter a course title");
@@ -25,7 +26,9 @@ public class CreateCourse implements Command {
         course.setTitle(title);
         course.setCourseStatus(CourseStatus.NOT_STARTED);
         courseDAO.create(course);
+        view.write(String.format("Course created with title - %s", title));
     }
+
     private String validate(String value) {
         while (value.trim().isEmpty()) {
             view.write("Please enter the correct title");
