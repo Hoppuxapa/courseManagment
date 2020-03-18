d88a8fe4602132f20ffb77fa25a3e8ca797d96ec
Ruslan <hoppuxapa@gmail.com>
2020-03-18 16:23:48 +0200

    Change commands to inject from a static class.

--- src/main/java/com/courses/management/common/commands/Exit.java
+++ src/main/java/com/courses/management/common/commands/Exit.java
@@ -1,7 +1,10 @@
+
 package com.courses.management.common.commands;
 
 import com.courses.management.common.Command;
 import com.courses.management.common.View;
+import com.courses.management.common.commands.util.Commands;
+import com.courses.management.common.commands.util.InputString;
 
 public class Exit implements Command {
     private View view;
@@ -12,11 +15,11 @@ public class Exit implements Command {
 
     @Override
     public String command() {
-        return "exit";
+        return Commands.EXIT;
     }
 
     @Override
-    public void process() {
+    public void process(InputString input) {
         view.write("Good bye!");
         System.exit(0);
     }
